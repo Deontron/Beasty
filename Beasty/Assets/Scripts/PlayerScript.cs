632839dc3f25e1c2b995dcb413e217884db376d9
Ahmet Çelik <ahmet.celik2605@outlook.com>
2023-02-20 15:34:50 +0300

    rotation

--- Beasty/Assets/Scripts/PlayerScript.cs
+++ Beasty/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,9 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private float movementSpeed;
 
     private Vector2 movementVector;
+    private Vector3 mousePos;
+    private Quaternion rotation;
+
     void Start()
     {
 
@@ -22,5 +25,9 @@ public class PlayerScript : MonoBehaviour
         movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         GetComponent<Rigidbody2D>().AddForce(movementVector * movementSpeed);
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rotation = Quaternion.LookRotation(mousePos - transform.position, transform.TransformDirection(-Vector3.forward));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
     }
 }
